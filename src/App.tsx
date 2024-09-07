d2eae0e6a9d1787935c636ebfd202a5d578ba10f
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-09-07 16:54:17 +0300

    refactor: update TechStack component and add title

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <div className="flex flex-col justify-center">
       <Hero />
-      <div className="flex items-center justify-center max-w-full max-h-full mt-4 mx-4 pt-6 pb-8  rounded-lg shadow-lg">
-        <TechStack all={true} />
+      <div className="flex flex-col gap-4 items-center justify-center max-w-full max-h-full mt-4 mx-4 pt-6 pb-8  rounded-lg shadow-lg">
+        <h2 className="text-4xl font-bold">My Tech Stack</h2>
+        <TechStack all={true} text="" />
       </div>
     </div>
   )
