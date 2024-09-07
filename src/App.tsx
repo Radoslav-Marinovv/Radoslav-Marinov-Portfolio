be92e7dc73423202ae0f080ca88c59863550fdff
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-09-07 16:46:43 +0300

    refactor: optimize BusinessCard component and Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import BusinessCard from "./components/BusinessCard/BusinessCard"
+import Hero from "./components/Hero/Hero"
 import TechStack from "./components/TechStack/TechStack"
 
 function App() {
 
   return (
     <div className="flex flex-col justify-center">
-      <BusinessCard />
-      <div className="flex items-center justify-center max-w-full max-h-full mt-4 mx-4 pt-6 pb-8 bg-gray-700 rounded-lg shadow-lg">
+      <Hero />
+      <div className="flex items-center justify-center max-w-full max-h-full mt-4 mx-4 pt-6 pb-8  rounded-lg shadow-lg">
         <TechStack all={true} />
       </div>
     </div>
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,78 @@
+import profilePicture from "../../assets/Images/profile-picture.png";
+import phoneLogo from "../../assets/Images/icon-phone.svg";
+import mailLogo from "../../assets/Images/icons-mail.png";
+import resume from "../../assets/Files/RadoslavMarinovResume.pdf";
+
+export default function Hero(): JSX.Element {
+
+  const anchorSocialStyle = "btn hover:text-blue-400";
+  const fieldsetStyle = "border border-white border-opacity-35 rounded-lg shadow-md";
+  const legendStyle = "xl:h-full ml-4 p-2 font-bold text-orange-500 text-lg";
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <img
+          src={profilePicture}
+          alt="profile picture"
+          width={384}
+          className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold ">Radoslav Marinov</h1>
+          <fieldset className={fieldsetStyle}>
+            <legend className={legendStyle}>Software Developer</legend>
+            <p className="p-6">
+              I am a software developer with a passion for web development. I have experience with JavaScript, TypeScript, React, Next.js, Node.js, and Firebase. I am also familiar with C# and have experience with HTML, CSS, and Git.
+            </p>
+          </fieldset>
+
+          <fieldset className={fieldsetStyle}>
+            <legend className={legendStyle}>You can find me here</legend>
+            <div className="">
+              <a
+                href="tel:+359888788687"
+                className={anchorSocialStyle}>
+                <img
+                  className="w-8 h-8"
+                  alt="phone logo"
+                  src={phoneLogo}
+                />
+                <p className="text-lg">Phone: +359 888 788 687</p>
+              </a>
+              <a
+                href="https://github.com/Radoslav-Marinovv"
+                target="_blank"
+                className={anchorSocialStyle}>
+                <img
+                  className="w-8 h-8"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                  alt="github logo" />
+                <p className="text-lg">GitHub: Radoslav-Marinovv</p>
+              </a>
+              <a
+                href="mailto:radoslav.marinov89@gmail.com"
+                className={anchorSocialStyle}>
+                <img
+                  className="w-8 h-8"
+                  src={mailLogo}
+                  alt="gmail logo" />
+                <p className="text-lg">Mail: radoslav.marinov89@gmail.com</p>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/radoslav-marinov-0b940689/"
+                target="_blank"
+                className={anchorSocialStyle}>
+                <img
+                  className="w-8 h-8"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
+                  alt="linkedin logo" />
+                <p className="text-lg">LinkedIn: Radoslav Marinov</p>
+              </a>
+            </div>
+            <div className={`w-full m-2 text-center`}> Or get my resume <a href={resume} download="RadoslavMarinovResume.pdf" className="btn btn-outline">Download</a></div>
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  );
+}
