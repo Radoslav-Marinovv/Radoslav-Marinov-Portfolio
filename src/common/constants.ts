c3e6bc0bcbd8ca537cfe2e35f177b89563b287f5
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-09-08 03:32:26 +0300

    refactor: update constants file with fieldset and legend styles

---
+++ src/common/constants.ts
@@ -0,0 +1,5 @@
+export const FIELDSET_STYLE =
+  'border border-white border-opacity-10 rounded-lg shadow-md';
+
+export const LEGEND_STYLE =
+  'xl:h-full ml-4 p-2 font-bold text-orange-500 text-lg';
