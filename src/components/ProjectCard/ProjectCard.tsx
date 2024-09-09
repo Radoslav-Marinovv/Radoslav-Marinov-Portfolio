4a53d9170100c609bfbd41f4be2aea8185408386
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-09-10 02:32:55 +0300

    refactor: Update ProjectCard component layout and styles

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -13,16 +13,15 @@ export default function ProjectCard({ title, description, techStack, github, web
 
   return (
     <>
-      <div className="card md:flex-row border bg-base-100 md:w-2/3 w-full shadow-xl my-8">
-        <figure className="border-1 shadow-xl md:hover:w-64 ">
+      <div className="card md:flex-row border bg-base-100 md:w-2/3 w-full md:max-h-screen shadow-xl my-8">
+        <figure className="border-1 shadow-xl md:hover:w-64 md:max-w-64">
           <img
             className="rounded-lg w-full h-64
              md:h-full md:w-64 md:object-cover md:object-left
              md:hover:absolute 
-             md:top-1 md:left-0
-             md:hover:top-4 md:hover:left-1 md:hover:z-10 
-             md:hover:w-full md:hover:object-fill 
-             md:transition-transform md:duration-1000 md:hover:scale-125 md:hover:ease-out md:ease-in"
+             md:hover:top-2 md:hover:left-0 md:hover:z-10 
+             md:hover:w-full md:hover:object-contain 
+             md:transition-all md:duration-1000 md:hover:scale-125 md:hover:ease-linear"
             src={image}
             alt={title + "image"} />
         </figure>
@@ -36,7 +35,7 @@ export default function ProjectCard({ title, description, techStack, github, web
           <div className="pt-4 card-bordered card-actions justify-start">
             {techStack}
           </div>
-          <div className="card-bordered card-actions justify-end">
+          <div className="card-bordered card-actions justify-center md:justify-end">
             {website && <fieldset className={FIELDSET_STYLE}>
               <legend className={LEGEND_STYLE}>Website</legend>
               <a
