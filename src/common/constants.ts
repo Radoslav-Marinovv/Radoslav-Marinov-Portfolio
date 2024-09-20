eee0adb1af33fb6eb982a6daec6ec144129e0255
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-09-21 01:43:47 +0300

    Refactor legend style in constants.ts

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,4 @@
 export const FIELDSET_STYLE =
   'border border-white border-opacity-10 rounded-lg shadow-md';
 
-export const LEGEND_STYLE =
-  'xl:h-full ml-4 p-2 font-bold text-orange-500 text-lg';
+export const LEGEND_STYLE = 'xl:h-full ml-4 p-2 font-bold text-lg';
