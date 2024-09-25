e917b8701ed759c1b5eb61ea4035d309d4f6c922
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-09-25 20:44:55 +0300

    Add JSDoc comments

--- src/components/TechStack/MyTechStack.tsx
+++ src/components/TechStack/MyTechStack.tsx
@@ -1,5 +1,8 @@
 import TechStack from "./TechStack";
-
+/**
+ * Renders a tech stack component with all tech stack logos.
+ * @returns {JSX.Element} The rendered tech stack component.
+ */
 export default function MyTechStack(): JSX.Element {
   return (
     <div className="items-center justify-center max-w-full max-h-full rounded-lg shadow-lg">
