cf14ecfa0bcfbe8b12460d4eec215c63506d829b
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-10-03 18:44:36 +0300

    Refactor ProjectCard component: Update image styling and remove unnecessary borders

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -25,14 +25,14 @@ export default function ProjectCard({ title, description, techStack, github, web
 
   return (
     <>
-      <div className="card md:flex-row bg-base-300 md:w-2/3 w-full md:max-h-screen shadow-xl my-8">
-        <figure className="border rounded-lg border-primary shadow-xl md:hover:w-64 md:max-w-64">
+      <div className="card md:flex-row odd:bg-base-300 bg-base-200 md:w-2/3 w-full md:max-h-screen shadow-xl my-8">
+        <figure className="rounded-lg shadow-xl md:hover:w-64 md:max-w-64">
           <img
             className="rounded-lg w-full h-64
              md:h-full md:w-64 md:object-cover
              md:hover:absolute md:hover:top-2 md:hover:left-0 md:hover:z-10 
              md:hover:w-fit md:hover:object-contain
-             md:hover:border md:hover:border-primary md:hover:rounded-lg md:hover:shadow-2xl  
+             md:hover:rounded-lg md:hover:shadow-2xl  
              md:transition-all md:duration-1000 md:hover:scale-125 md:hover:ease-linear"
             src={image}
             alt={title + "image"} />
@@ -41,13 +41,13 @@ export default function ProjectCard({ title, description, techStack, github, web
           <h2 className="card-title">
             {title}
           </h2>
-          <p className="card-bordered font-medium">
+          <p className="font-medium text-justify">
             {description}
           </p>
-          <div className="pt-4 card-bordered card-actions justify-start">
+          <div className="pt-4 card-actions justify-start">
             <TechStack {...techStack} />
           </div>
-          <div className="card-bordered card-actions justify-center md:justify-end">
+          <div className="card-actions justify-center md:justify-end">
             {website && <fieldset className={FIELDSET_STYLE}>
               <legend className={LEGEND_STYLE}>Website</legend>
               <a
