25047863e61534674e44bd063dee73715da2a3e8
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-10-04 01:16:12 +0300

    Refactor Education component: Add animation to education cards

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,5 +1,12 @@
+import { useRef } from "react";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
 import EducationCard from "./EducationCard.tsx";
 import { EDUCATION_LIST } from "../../data/education.ts"
+
 type EducationCardProps = {
   id: string;
   title: string;
@@ -13,12 +20,45 @@ type EducationCardProps = {
  * A component that displays information about the education
  * @param educationCards - an array of objects with information about the education
  * @returns - a list of components that displays information about the education
- */
+*/
 export default function Education(): JSX.Element {
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const cardRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.fromTo(".education",
+      {
+        x: gsap.utils.random(-50, 100, 1),
+        y: gsap.utils.random(-50, 100, 1),
+        skewX: gsap.utils.random(-90, 90, 1),
+        skewY: gsap.utils.random(-90, 90, 1),
+        opacity: 0,
+        scale: 0,
+      },
+      {
+        x: 0,
+        y: 0,
+        skewX: 0,
+        skewY: 0,
+        opacity: 1,
+        scale: 1,
+        duration: 2.5,
+        repeat: 0,
+        yoyo: true,
+        ease: "elastic",
+        stagger: {
+          each: 0.8,
+        },
+      }
+    );
+  }, { scope: cardRef, revertOnUpdate: true });
+
   return (
-    <div className="flex flex-col justify-center align-middle text-center">
+    <div className="flex flex-col justify-center align-middle text-center ">
       <h2 className="text-4xl font-bold py-8">Education</h2>
-      <div className="flex flex-wrap justify-center align-baseline">
+      <div ref={cardRef} className="flex flex-wrap justify-center align-baseline">
         {EDUCATION_LIST.map((item: EducationCardProps) => (
           <EducationCard
             key={item.id}
--- src/components/Education/EducationCard.tsx
+++ src/components/Education/EducationCard.tsx
@@ -19,7 +19,7 @@ type EducationCardProps = {
 export default function EducationCard({ title, degree, date, location, logoImage }: EducationCardProps): JSX.Element {
 
   return (
-    <div className="card m-2 bg-base-300 md:flex-row md:w-2/3 w-full md:max-h-screen shadow-xl my-8 text-left">
+    <div className="education card m-2 bg-base-300 md:flex-row md:w-2/3 w-full md:max-h-screen shadow-xl my-8 text-left">
       <figure className="shadow-xl md:hover:w-64 md:max-w-64 p-4">
         <img
           className="rounded-lg w-full h-64
