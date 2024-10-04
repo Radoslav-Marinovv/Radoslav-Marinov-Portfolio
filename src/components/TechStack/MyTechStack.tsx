758f3931050baa371057d931026ada030ea1c86c
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-10-05 02:08:22 +0300

    Refactor MyTechStack component: Add id to tech stack container

--- src/components/TechStack/MyTechStack.tsx
+++ src/components/TechStack/MyTechStack.tsx
@@ -5,7 +5,9 @@ import TechStack from "./TechStack";
  */
 export default function MyTechStack(): JSX.Element {
   return (
-    <div className="items-center bg-base-300 justify-center max-w-full max-h-full rounded-lg shadow-lg">
+    <div
+      id="tech-stack"
+      className="items-center bg-base-300 justify-center max-w-full max-h-full rounded-lg shadow-lg">
       <h2 className="p-8 text-4xl font-bold text-center">My Tech Stack</h2>
       <TechStack all={true} text="" />
     </div>
