37b2093a75770819c87a703f46b45152d4ab8431
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-10-06 01:09:04 +0300

    Refactor index.html: Update favicon and page title

--- src/hoc/Layout/Footer/Footer.tsx
+++ src/hoc/Layout/Footer/Footer.tsx
@@ -1,36 +1,59 @@
+import { useEffect, useState } from "react";
 
 /**
  * 
  * @returns - the footer of the application
  */
 export default function Footer(): JSX.Element {
+
+  const [currentPath, setCurrentPath] = useState(window.location.hash);
+
+  useEffect(() => {
+    const handleLocationChange = () => {
+      setCurrentPath(window.location.hash);
+    };
+
+    window.addEventListener('scroll', handleLocationChange);
+
+    return () => {
+      window.removeEventListener('scroll', handleLocationChange);
+    };
+  }, [currentPath]);
+
   return (
-    <div className="navbar bg-base-100 sticky bottom-0 left-0 mt-auto">
+    <footer className="navbar bg-base-100 sticky bottom-0 left-0 mt-auto z-50">
       <a
-        className="btn btn-ghost md:text-xl"
+        className={`btn btn-ghost md:text-xl ${currentPath === "" && "btn-outline" || currentPath === "#Portfolio" && "btn-outline"}`}
         href={"/"}
+        title="Info"
+        onClick={() => { document.title = "Radoslav Marinov | Portfolio"; }}
       >
         Info
       </a>
       <a
-        className="btn btn-ghost md:text-xl"
-        href={"/#education"}
+        className={`btn btn-ghost md:text-xl ${currentPath === "#Education" && "btn-outline"}`}
+        href={"/#Education"}
+        title="Education"
+        onClick={() => { document.title = "Radoslav Marinov | Education"; }}
       >
         Education
       </a>
       <a
-        className="btn btn-ghost md:text-xl"
-        href={"/#tech-stack"}
-
+        className={`btn btn-ghost md:text-xl ${currentPath === "#Tech-Stack" && "btn-outline"}`}
+        href={"/#Tech-Stack"}
+        title="Tech-Stack"
+        onClick={() => { document.title = "Radoslav Marinov | Tech Stack"; }}
       >
         Tech Stack
       </a>
       <a
-        className="btn btn-ghost md:text-xl"
-        href={"/#projects"}
+        className={`btn btn-ghost md:text-xl ${currentPath === "#Projects" && "btn-outline"}`}
+        href={"/#Projects"}
+        title="Projects"
+        onClick={() => { document.title = "Radoslav Marinov | Projects"; }}
       >
         Projects
       </a>
-    </div>
+    </footer>
   );
 } 
--- src/hoc/Layout/Layout.tsx
+++ src/hoc/Layout/Layout.tsx
@@ -1,14 +1,57 @@
+import { useEffect, useLayoutEffect, useState } from "react"
+
 import Footer from "./Footer/Footer";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 /**
- * A component that displays the main content of the application
+ * A component that displays the main content of the application and listens for scroll events.
  * @param children - the content of the application
  * @returns - the main content of the application
  */
 export default function Layout({ children }: LayoutProps): JSX.Element {
+
+  const [activeSection, setActiveSection] = useState("");
+
+  useLayoutEffect(() => {
+    if (window.screenY === 0) {
+      setActiveSection("| Portfolio");
+    }
+  }, []);
+
+  useEffect(() => {
+    document.title = `Radoslav Marinov ${activeSection}`;
+    if (activeSection === "" || activeSection === "#portfolio") {
+      window.history.pushState(null, "", ``);
+    } else {
+      window.history.pushState(null, "", `#${activeSection.replace("| ", "").replace(/ /g, "-")}`);
+    }
+
+  }, [activeSection]);
+
+  const handleScroll = () => {
+    const sections = document.querySelectorAll("section");
+    let currentSection: string | null = "";
+
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top < window.innerHeight / 2) {
+        currentSection = "| " + section.getAttribute("id")?.replace(/-/g, " ");
+      }
+    });
+
+    setActiveSection(currentSection);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <main className="flex flex-col gap-8">{children}</main>
