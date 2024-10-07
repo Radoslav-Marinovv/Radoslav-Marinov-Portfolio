51b9300dfd0b91ba6ac64534323fe0d71308922c
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-10-07 18:41:41 +0300

    Refactor App component: Import gsap and ScrollTrigger plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import Hero from "./components/Hero/Hero"
-import Education from "./components/Education/Education"
-import MyTechStack from "./components/TechStack/MyTechStack.tsx"
-import Projects from "./components/ProjectCard/Projects.tsx"
 import Layout from "./hoc/Layout/Layout.tsx"
 
-function App() {
+import Hero from "./components/Hero/Hero"
+import Education from "./components/Education/Education"
+import Projects from "./components/ProjectCard/Projects.tsx"
+import MyTechStack from "./components/TechStack/MyTechStack.tsx"
 
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+
+function App() {
+  gsap.registerPlugin(ScrollTrigger);
   return (
     <Layout>
       <Hero />
