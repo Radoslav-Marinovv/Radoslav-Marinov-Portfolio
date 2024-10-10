bcd80c231b13648f03b995c84a278f5869958995
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-10-10 19:29:32 +0300

    Refactor Education component: Remove unused import and update dependencies

--- src/components/Education/EducationCard.tsx
+++ src/components/Education/EducationCard.tsx
@@ -1,8 +1,3 @@
-import { useRef } from "react";
-
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-
 import { FIELDSET_STYLE, LEGEND_STYLE } from "../../common/constants";
 
 type EducationCardProps = {
@@ -24,53 +19,8 @@ type EducationCardProps = {
  */
 export default function EducationCard({ title, degree, date, location, logoImage }: EducationCardProps): JSX.Element {
 
-  const singleCardRef = useRef(null);
-  useGSAP(() => {
-    gsap.fromTo(singleCardRef.current,
-      {
-        width: "33%",
-
-      },
-      {
-        scrollTrigger: {
-          trigger: singleCardRef.current,
-          start: "clamp(top 80%)",
-          end: "clamp(bottom 20%)",
-        },
-        width: "45%",
-        opacity: 1,
-        duration: 1,
-      });
-    gsap.fromTo('.picture',
-      {
-        x: 500,
-        opacity: 0,
-      },
-      {
-        x: 0,
-        gridColumn: "1 / 2",
-        opacity: 1,
-        delay: 1.5,
-        stagger: 0.5,
-      });
-    gsap.fromTo('.body',
-      {
-        width: "0%",
-        x: -500,
-        opacity: 0,
-      },
-      {
-        x: 0,
-        opacity: 1,
-        delay: 1.5,
-        stagger: 0.5,
-      });
-
-  }, { scope: singleCardRef, revertOnUpdate: true });
-
   return (
     <div
-      ref={singleCardRef}
       id="educationCard"
       className="card bg-base-300 md:flex-row w-screen md:w-1/3 shadow-xl md:mx-8 my-4 text-sm text-justify">
       <figure className="picture shadow-xl h-full p-4">
