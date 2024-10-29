850b4e88e4a4a5ecdb8839d0a1febb065de468da
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-10-29 20:24:25 +0200

    Refactor Projects component: Add animation using GSAP library

--- src/components/ProjectCard/Projects.tsx
+++ src/components/ProjectCard/Projects.tsx
@@ -1,18 +1,50 @@
+import { useRef } from "react";
 
-import { PROJECTS } from "../../data/projects";
 import ProjectCard from "./ProjectCard";
+import { PROJECTS } from "../../data/projects";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 /**
  * A component that displays information about the projects.
  * @returns {JSX.Element} The rendered projects component.
  */
 export default function Projects(): JSX.Element {
+
+  const projectsRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.fromTo(".card",
+      {
+        x: -2000,
+        y: 0,
+        opacity: 0,
+        scale: 0,
+      },
+      {
+        immediateRender: false,
+        scrollTrigger: {
+          trigger: projectsRef.current,
+          toggleActions: "restart reverse restart reverse",
+        },
+        duration: 2.5,
+        ease: "elastic.inOut",
+        x: 0,
+        y: 0,
+        opacity: 1,
+        stagger: 0.5,
+        scale: 1,
+      }
+    );
+  }, { scope: projectsRef, revertOnUpdate: true });
   return (
     <section
       id="Projects"
+      ref={projectsRef}
       className="flex flex-col text-center">
       <h2 className="text-4xl font-bold py-8">Projects</h2>
-      <div className="flex flex-col p-2 align-middle justify-center items-center">
+      <div className="flex flex-col p-2 mb-4 align-middle justify-center items-center">
         {PROJECTS.map((project) => (
           <ProjectCard
             key={project.id}
