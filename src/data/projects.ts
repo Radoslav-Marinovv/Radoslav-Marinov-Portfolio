d396631ac44a539267d07db1107e38556714e31e
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-04-11 16:18:06 +0300

    Add Currency Converter project and update project IDs in projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,26 @@
 export const PROJECTS = [
   {
-    id: '1',
+    id: '7',
+    title: 'Currency Converter',
+    description:
+      'Convert from USD (Base) to 33 other currencies, learned a lot from it. Redux, Express.js, the new TailwindCSS(v4) implementation for the first time here. Learned how to use Vercel configuration file so i can run both front-end and back-end (as Vercel function) on same deployment.',
+    techStack: {
+      react: true,
+      js: true,
+      ts: true,
+      express: true,
+      redux: true,
+      mongodb: true,
+      tailwind: true,
+      daisyui: true,
+      json: true,
+    },
+    github: 'https://github.com/Radoslav-Marinovv/currency-converter',
+    website: 'https://currency-converter-brown-rho.vercel.app/',
+    image: './assets/images/projects/currency-converter.jpg',
+  },
+  {
+    id: '6',
     title: 'Fitness tracking application: Motion Mate',
     description:
       'As part of a team, I was involved in creating the fitness tracking application, where users can track their daily exercises, such as calories burned, exercises done, or other activities. Users can set their goals, track their progress, add friends (and see their progress as well). I was responsible for creating the database connection, the GitHub repository, and building the functionality and style for the project.',
@@ -17,7 +37,7 @@ export const PROJECTS = [
     image: './assets/images/projects/motion-mate.jpg',
   },
   {
-    id: '2',
+    id: '5',
     title: 'Forum: Tasty Recipe Talk',
     description:
       'As part of a team, I was involved in creating the food recipe forum, creating the database connection, the GitHub repository. Building functionality and style of the project',
@@ -32,7 +52,7 @@ export const PROJECTS = [
     image: './assets/images/projects/tasty-recipe-talk.jpg',
   },
   {
-    id: '3',
+    id: '4',
     title: 'Groshev & Partners',
     description:
       "Led client communication to understand and refine project requirements, planned the optimal technologies for the project, and outlined the steps needed to meet the client's criteria.",
@@ -50,7 +70,7 @@ export const PROJECTS = [
     image: './assets/images/projects/lawyers.jpg',
   },
   {
-    id: '4',
+    id: '3',
     title: 'TWITTER LIKE SPA',
     description:
       'React with TypeScript application created by Vite. Presenting mock data on the main page by fetching it from a custom hook. Validation before creating a new post (check for the minimum - maximum length of the post). Communication between user and post objects and update likes and posts at the user object.',
@@ -66,7 +86,7 @@ export const PROJECTS = [
     image: './assets/images/projects/like-comment.jpg',
   },
   {
-    id: '5',
+    id: '2',
     title: 'Tic Tac Toe',
     description:
       'Tic tac toe game written on React with Type Script and deployed on Github Pages.',
@@ -76,7 +96,7 @@ export const PROJECTS = [
     image: './assets/images/projects/tic-tac-toe-light.jpg',
   },
   {
-    id: '6',
+    id: '1',
     title: 'Portfolio (old)',
     description:
       'The portfolio is created with Next.js and a library called Nextra that gives flexible generation of content based on MDX files.',
